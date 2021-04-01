dbe00e2beac2091a669637d4afd8e43f77fa9724
JamesPuli <pulijames@outlook.com>
2021-04-01 22:10:10 +1100

    Stone Graphic Overhaul

--- frontend/src/components/game_components/Mat.js
+++ frontend/src/components/game_components/Mat.js
@@ -23,8 +23,8 @@ export const Mat = ({ stoneState, onClickMat, onClickStone, activeStone }) => {
     return (
         <div style={style}>
             <div style={{ position: 'absolute', width: '100%', height: '100%', display: 'flex' }}>
-                <div style={{ zIndex: 101, width: '50%', height: '100%' }} onClick={() => onClickMat('left')} />
-                <div style={{ zIndex: 101, width: '50%', height: '100%' }} onClick={() => onClickMat('right')} />
+                <div style={{ zIndex: -101, width: '50%', height: '100%' }} onClick={() => onClickMat('left')} />
+                <div style={{ zIndex: -101, width: '50%', height: '100%' }} onClick={() => onClickMat('right')} />
             </div>
             {matStones.map((stone, i) => (
                 <Stone key={`stone_${i}`} name={stone} onClick={() => onClickStone(stone)} isActive={stone === activeStone} isHidden={stoneState[stone].hidden} />
--- frontend/src/components/game_components/Stone.js
+++ frontend/src/components/game_components/Stone.js
@@ -1,46 +1,56 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+
+const activeColor = '#0E77F1';
+const inactiveColor ='#7BB5F8';
 
 const styles = {
     root: isActive => ({
         width: '100px',
         height: '100px',
-        backgroundColor: isActive ? 'grey' : '#fff',
+        backgroundColor: '#eee',
         borderRadius: '50%',
         zIndex: 102,
     }),
-    ring1: {
+    ring: (isActive) => ({
         position: 'relative',
-        backgroundColor: 'green',
+        backgroundColor: isActive ? activeColor : inactiveColor,
+        boxShadow: isActive ? '0 0 2px 2px #7BB5F8' : 'inset 0.5px 0.5px 0.5px 0.5px #2c6ac5',
         width: 'calc(100% - 10px)',
         height: 'calc(100% - 10px)',
         top: 5,
         left: 5,
         borderRadius: '50%',
-    },
-    ring2: (isActive) => ({
+    }),
+    innerCircle: (isActive) => ({
         position: 'relative',
-        backgroundColor: isActive ? 'grey' : '#fff',
+        backgroundColor: '#eee',
+        boxShadow: isActive ? '0 0 2px 2px #7BB5F8' : '',
         width: 'calc(100% - 8px)',
         height: 'calc(100% - 8px)',
         top: '4px',
         left: '4px',
         borderRadius: '50%',
     }),
-    text: {
+    text: (isActive) => ({
         paddingTop: '24px',
         fontSize: '25px',
         margin: '0px',
-        color: '#007acc',
-        textShadow: '2px 2px 2px #444'
-    }
+        color: isActive ? activeColor : inactiveColor,
+        textShadow: isActive ? '0 0 2px 2px #7BB5F8' : '-0.5px -0.5px 1px #2c6ac5'
+    })
 }
 
 export const Stone = ({ name, onClick, isActive, isHidden }) => (
-    isHidden ? (<div style={styles.root(isActive)} onClick={onClick} ></div>) : (<div style={styles.root(isActive)} onClick={onClick}>
-        <div style={styles.ring1}>
-            <div style={styles.ring2(isActive)}>
-                <p style={styles.text}>{name}</p>
-            </div>
-        </div>
-    </div>)
+    <div style={{marginTop: isActive ? '5px' : '0px', marginLeft: '2px', marginRight:'2px'}}>
+        {   isHidden ? (
+            <Paper style={ styles.root(isActive) } onClick={ onClick }></Paper>) : (<Paper style={ styles.root(isActive) } onClick={ onClick } elevation={ isActive ? 7 : 1 }>
+                <Paper style={ styles.ring(isActive) } elevation={ 0 }>
+                    <Paper style={ styles.innerCircle(isActive) } elevation={ 2 }>
+                        <p style={ styles.text(isActive) }>{ name }</p>
+                    </Paper>
+                </Paper>
+            </Paper>)
+        }
+    </div>
 );
